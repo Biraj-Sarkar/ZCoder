63285c5adced4b90db6fd57703714e85d40909d1
jitbarui1720 <j.barui@iitg.ac.in>
2025-06-08 18:22:28 +0530

    Update Solution.js

--- models/Solution.js
+++ models/Solution.js
@@ -2,42 +2,59 @@ const mongoose = require('mongoose');
 
 // Solution Schema Definition
 const SolutionSchema = new mongoose.Schema({
-    problem: {
+  problem: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Problem',
+    required: [true, 'Problem reference is required']
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User reference is required']
+  },
+  code: {
+    type: String,
+    required: [true, 'Code is required']
+  },
+  language: {
+    type: String,
+    required: [true, 'Language is required'],
+    enum: ['JavaScript', 'Python', 'Java', 'C++', 'Ruby']
+  },
+  status: {
+    type: String,
+    enum: ['Accepted', 'Wrong Answer', 'Runtime Error', 'Time Limit Exceeded'],
+    default: 'Accepted'
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  },
+  testResults: [
+    {
+      input: { type: String, required: true },
+      expectedOutput: { type: String, required: true },
+      userOutput: { type: String, required: true },
+      passed: { type: Boolean, required: true }
+    }
+  ],
+  comments: [
+    {
+      user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Problem',
-        required: [true, 'Problem reference is required']
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User ',
-        required: [true, 'User  reference is required']
-    },
-    code: {
+        ref: 'User',
+        required: true
+      },
+      text: {
         type: String,
-        required: [true, 'Code is required']
-    },
-    language: {
-        type: String,
-        required: [true, 'Language is required'],
-        enum: ['JavaScript', 'Python', 'Java', 'C++', 'Ruby'], // Add supported languages
-    },
-    comments: [
-        {
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User ',
-                required: [true, 'User  reference is required']
-            },
-            text: {
-                type: String,
-                required: [true, 'Comment text is required']
-            },
-            createdAt: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ],
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Solution', SolutionSchema);
