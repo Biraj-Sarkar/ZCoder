59058990e0181af399644dfdb4ca203d29471de8
jitbarui1720 <j.barui@iitg.ac.in>
2025-06-08 17:35:56 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,56 +1,75 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-// User Schema Definition
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'Username is required'],
-        unique: true,
-        minlength: [3, 'Username must be at least 3 characters long'],
-        maxlength: [30, 'Username cannot exceed 30 characters']
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        match: [/.+@.+\..+/, 'Please enter a valid email address']
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minlength: [6, 'Password must be at least 6 characters long']
-    },
-    profilePicture: {
-        type: String, // URL to profile picture
-        default: 'https://via.placeholder.com/150'
-    },
-    bookmarkedProblems: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Problem'
-        }
-    ]
+username: {
+type: String,
+required: [true, 'Username is required'],
+unique: true,
+minlength: [3, 'Username must be at least 3 characters long'],
+maxlength: [30, 'Username cannot exceed 30 characters']
+},
+email: {
+type: String,
+required: [true, 'Email is required'],
+unique: true,
+match: [/.+@.+..+/, 'Please enter a valid email address']
+},
+password: {
+type: String,
+required: [true, 'Password is required'],
+minlength: [6, 'Password must be at least 6 characters long']
+},
+profilePicture: {
+type: String,
+default: 'https://via.placeholder.com/150'
+},
+role: {
+type: String,
+enum: ['user', 'admin'],
+default: 'user'
+},
+bio: {
+type: String,
+default: ''
+},
+country: {
+type: String,
+default: ''
+},
+lastLogin: {
+type: Date,
+default: null
+},
+solvedProblems: [{
+type: mongoose.Schema.Types.ObjectId,
+ref: 'Problem'
+}],
+submissions: [{
+type: mongoose.Schema.Types.ObjectId,
+ref: 'Submission'
+}],
+bookmarkedProblems: [{
+type: mongoose.Schema.Types.ObjectId,
+ref: 'Problem'
+}]
 }, { timestamps: true });
 
 // Hash password before saving
 UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next(); // Fixed: Added return statement
-    }
-    
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+if (!this.isModified('password')) return next();
+try {
+const salt = await bcrypt.genSalt(10);
+this.password = await bcrypt.hash(this.password, salt);
+next();
+} catch (err) {
+next(err);
+}
 });
 
-// Method to compare passwords
+// Compare password
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model('User', UserSchema);
